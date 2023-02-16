22bff68097b26dd1a98b3d77b6322ef848e85c02
irinazharkova <iirinache@gmail.com>
2023-02-16 20:00:55 +0300

    исправление после ревью. Добавила метод distance в класс Point, поправила тесты

--- sandbox/src/main/java/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,11 +4,8 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     Point p1 = new Point(5, -1);
     Point p2 = new Point(1, 4);
-    System.out.println("Расстояние между точками (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") равно " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    System.out.println("Расстояние между точками (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") равно "
+            + p1.distance(p2));
   }
 }
 
--- sandbox/src/test/java/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/stqa/pft/sandbox/DistanceTests.java
@@ -3,33 +3,32 @@ package stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static stqa.pft.sandbox.MyFirstProgram.distance;
 
 public class DistanceTests {
   @Test
   public void testDistance0(){
     Point p1 = new Point(0, 0);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(distance(p1, p2),0.0);
+    Assert.assertEquals(p1.distance(p2),0.0);
   }
   @Test
   public void testDistance1(){
     Point p1 = new Point(1, 1);
     Point p2 = new Point(0, 1);
-    Assert.assertEquals(distance(p1, p2),1.0);
+    Assert.assertEquals(p1.distance(p2),1.0);
   }
 
   @Test
   public void testDistance2(){
     Point p1 = new Point(1, 1);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(distance(p1, p2),1.4142135623730951);
+    Assert.assertEquals(p1.distance(p2),1.4142135623730951);
   }
 
   @Test
   public void testDistance3(){
     Point p1 = new Point(-1, -1);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(distance(p1, p2),1.4142135623730951);
+    Assert.assertEquals(p1.distance(p2),1.4142135623730951);
   }
 }
