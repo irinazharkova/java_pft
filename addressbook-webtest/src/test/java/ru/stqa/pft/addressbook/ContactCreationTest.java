ab38b55926b216d74a0251cc0e0d983d395608c4
irinazharkova <iirinache@gmail.com>
2023-02-24 01:31:55 +0300

    edit test name

--- addressbook-webtest/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-webtest/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -21,7 +21,7 @@ public class ContactCreationTest {
   }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
+  public void testContactCreation() throws Exception {
     gotoContactCreationPage();
     fillContactForm(new ContactData("first", "midle", "last", "nick", "title", "address", "9999999", "99999", "test@test.ru"));
     submitContactCreation();
